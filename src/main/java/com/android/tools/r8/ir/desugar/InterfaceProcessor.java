69dbb169e769daca19082ee5e27a68f692124b26
Mikaël Peltier <mikaelpeltier@google.com>
2017-05-24 10:16:26 +0200

    Companion class must be public
    - Companion classes used when we desugar default methods must be
    public so moved methods can be called from anywhere, otherwise
    runtime can generate IllegalAccessError in some cases.
    
    Change-Id: I3496934baf3c8f56db53d6e598fa1405e71e03b0

--- src/main/java/com/android/tools/r8/ir/desugar/InterfaceProcessor.java
+++ src/main/java/com/android/tools/r8/ir/desugar/InterfaceProcessor.java
@@ -121,6 +121,8 @@ final class InterfaceProcessor {
     companionClassFlags.unsetInterface();
     companionClassFlags.setFinal();
     companionClassFlags.setSynthetic();
+    // Companion class must be public so moved methods can be called from anywhere.
+    companionClassFlags.setPublic();
 
     // Create companion class.
     DexType companionClassType = rewriter.getCompanionClassType(iface.type);
