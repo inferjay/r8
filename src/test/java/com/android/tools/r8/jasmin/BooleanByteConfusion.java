9586b1b8bfa3c0f29284566ab65b179424b6addc
Mads Ager <ager@google.com>
2017-06-29 15:21:11 +0200

    Remove incorrect assertion in new array rewriting.
    Integers that are outside the 8 bit area can be written to byte
    arrays and are truncated. The filled-array data optimization
    will do proper truncation of the value and is therefore already
    correct. We just have to remove the assertion.
    
    R=sgjesse@google.com
    
    Change-Id: I94ee98ee3af598ca5abdc8e5e0a5887add1d12f4

--- src/test/java/com/android/tools/r8/jasmin/BooleanByteConfusion.java
+++ src/test/java/com/android/tools/r8/jasmin/BooleanByteConfusion.java
@@ -3,7 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 package com.android.tools.r8.jasmin;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
